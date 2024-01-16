9bf8bf7401af945d551ed0d4fa5036a7c876234f
KayakOnWheels <115392859+KayakOnWheels@users.noreply.github.com>
2024-01-16 19:33:01 +0100

    6.2 added module user and calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        /*SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        } */
+
+        Calculator calculator = new Calculator();
+
+        if(calculator.add(5,2) == 7) {
+            System.out.println("test OK");
+        }
+        else {
+            System.out.println("Error");
+        }
+
+        if(calculator.substarct(5,2) == 3) {
+            System.out.println("test OK");
+        }
+        else {
+            System.out.println("Error");
+        }
     }
 }
