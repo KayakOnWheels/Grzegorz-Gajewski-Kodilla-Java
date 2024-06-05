4fed51a2f30961588be85c2fab52916708b576ab
KayakOnWheels <115392859+KayakOnWheels@users.noreply.github.com>
2024-06-05 22:27:02 +0200

    Added aspect to OrderFacade

--- patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -3,8 +3,12 @@ import com.kodilla.patterns2.facade.ShopService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Service;
+
 import java.math.BigDecimal;
+
+@EnableAspectJAutoProxy
 @Service
 public class OrderFacade {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
@@ -13,6 +17,7 @@ public class OrderFacade {
     public OrderFacade(ShopService shopService) {
         this.shopService = shopService;
     }
+
     public void processOrder(final OrderDto order, final Long userId) throws OrderProcessingException {
         boolean wasError = false;
         Long orderId = shopService.openOrder(userId);
